27ef180de11e05272361eedca66ec9b441278966
Mikhail Kedzel <mishanx873@gmail.com>
2020-02-15 14:08:27 +0300

    fix: router now works

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -1,31 +1,31 @@
 import * as React from 'react';
 import Header from '../Header';
-import { Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 
 const history = createBrowserHistory();
 
 function App() {
-	return (
-		<div>
-			<Router history={history}>
-				<Header />
+    return (
+        <div>
+            <Router>
+                <Header />
 
-				<Switch>
-					<Route
-						path='/'
-						render={() => <h2>Main</h2>}
-						exact
-					/>
-					<Route
-						path='/search'
-						render={() => <h2>Search</h2>}
-						exact
-					/>
-				</Switch>
-			</Router>
-		</div>
-	);
+                <Switch>
+                    <Route
+                        path='/'
+                        render={() => <h2>Main</h2>}
+                        exact
+                    />
+                    <Route
+                        path='/search'
+                        render={() => <h2>Search</h2>}
+                        exact
+                    />
+                </Switch>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
