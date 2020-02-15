672867e168c0806387b9fb90fbf9388379fa2834
madjack <kavanokami@gmail.com>
2020-02-15 20:16:38 +0400

    feat: add /architect route

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -2,30 +2,24 @@ import * as React from 'react';
 import Header from '../Header';
 import Youtube from '../Youtube';
 import HomePage from '../../pages/Home-page/index';
+import ArchitectPage from '../../pages/Architect-page';
 
 import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
-	return (
-		<div>
-			<Router>
-				<Header />
+  return (
+    <div>
+      <Router>
+        <Header />
 
-				<Switch>
-					<Route
-						path='/'
-						render={() => <HomePage />}
-						exact
-					/>
-					<Route
-						path='/search'
-						render={() => <Youtube />}
-						exact
-					/>
-				</Switch>
-			</Router>
-		</div>
-	);
+        <Switch>
+          <Route path="/" render={() => <HomePage />} exact />
+          <Route path="/search" render={() => <Youtube />} exact />
+          <Route path="/architect/:id" render={() => <ArchitectPage />} />
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
