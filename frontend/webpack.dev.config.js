109843050d81b22a256d1c493f62bd1e78453dd9
Vitali Satsura <vitali.satsura@gmail.com>
2020-02-15 19:42:22 +0300

    feat: add Biography

--- frontend/webpack.dev.config.js
+++ frontend/webpack.dev.config.js
@@ -46,7 +46,7 @@ module.exports = {
                 loader: "source-map-loader"
             },
             {
-                test: /\.(scss)$/,
+                test: /\.(sa|sc|c)ss$/,
                 use: [{
                     loader: 'style-loader', // inject CSS to page
                 }, {
