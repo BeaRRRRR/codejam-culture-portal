20ca63d0d7c89e9ebf463a919e6f6d62e8d4df50
wlukla <vladislav.danilochkin@gmail.com>
2020-02-15 14:20:24 +0200

    feat: add youtube player

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -4,16 +4,16 @@ import { Link } from 'react-router-dom';
 import './Header.scss';
 
 const Header = () => (
-	<header className="header">
+	<header className='header'>
 		<Link to='/' >
-			<h1 className="header__heading">Architects of Belarus</h1>
+			<h1 className='header__heading'>Architects of Belarus</h1>
 		</Link>
-		<nav className="navbar">
-			<ul className="navbar__list">
-				<li className="navbar__list-item">
+		<nav className='navbar'>
+			<ul className='navbar__list'>
+				<li className='navbar__list-item'>
 					<Link to='/'>Home</Link>
 				</li>
-				<li className="navbar__list-item">
+				<li className='navbar__list-item'>
 					<Link to='/search'>Search</Link>
 				</li>
 			</ul>
