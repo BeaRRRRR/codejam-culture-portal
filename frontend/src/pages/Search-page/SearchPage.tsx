2585ce04667432514df82a3b6b4195cced58c3c4
madjack <kavanokami@gmail.com>
2020-02-15 22:32:33 +0400

    feat: add SearchPage

---
+++ frontend/src/pages/Search-page/SearchPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import Search from "../../components/Search";
+
+import "./SearchPage.scss";
+
+function SearchPage() {
+  return <Search />;
+}
+
+export default SearchPage;
