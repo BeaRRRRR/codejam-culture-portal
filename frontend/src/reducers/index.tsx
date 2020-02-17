a62abd7cca62e2af6f54b5412de37c9d4c48a06d
madjack <kavanokami@gmail.com>
2020-02-17 13:23:38 +0400

    feat: add rootReducer and types

--- frontend/src/reducers/index.tsx
+++ frontend/src/reducers/index.tsx
@@ -1,22 +1,32 @@
-const defaultState = {
-	authorsList: [],
-	author: {}
-};
+import { RootAction } from "../store/types";
+import { actionTypes } from "../actions";
 
-interface State {
-	authorsList: [];
-	author: {};
-	// add other fields
+interface AuthorModel {
+	id: string;
+	name: string;
 }
 
-const reducer = (state: State, action: object) => {
+interface StateModel {
+	authorsList: Array<AuthorModel>;
+	author: AuthorModel;
+}
+
+export const initialState: StateModel = {
+	authorsList: [],
+	author: { id: "1a", name: "test architect" }
+};
+
+export const rootReducer = (
+	state: StateModel = initialState,
+	action: RootAction
+) => {
 	switch (action.type) {
-		case "FETCH_AUTHORS_LIST":
+		case actionTypes.FETCH_AUTHORS_LIST:
 			return {
 				...state,
 				authorsList: action.payload
 			};
-		case "FETCH_AUTHOR":
+		case actionTypes.FETCH_AUTHOR:
 			return {
 				...state,
 				author: action.payload
@@ -25,5 +35,3 @@ const reducer = (state: State, action: object) => {
 			return state;
 	}
 };
-
-export default reducer;
--- frontend/src/store/types.ts
+++ frontend/src/store/types.ts
@@ -1,3 +1,5 @@
 import { StateType, ActionType } from "typesafe-actions";
+import { rootReducer } from "../reducers";
 
+export type ReducerState = StateType<typeof rootReducer>;
 export type RootAction = ActionType<typeof import("../actions/index")>;
