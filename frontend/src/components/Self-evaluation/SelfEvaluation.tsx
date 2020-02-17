9a82c843044afd7e21b2d6309e7cca267720d32c
wlukla <vladislav.danilochkin@gmail.com>
2020-02-17 11:09:53 +0200

    feat: update render in SelfEvaluation

--- frontend/src/components/Self-evaluation/SelfEvaluation.tsx
+++ frontend/src/components/Self-evaluation/SelfEvaluation.tsx
@@ -1,43 +1,46 @@
 import React from 'react';
 import './SelfEvaluation.scss';
 import SelfEvaluationListItem from '../Self-evaluation-list-item';
+import { Paper } from '@material-ui/core';
 
-function SelfEvaluation() {
+type dataScope = {
+	scope: string,
+	points: number,
+	items: Array<dataItem>
+};
+
+type dataItem = {
+	points: number,
+	text: string
+};
+
+type Props = {
+	data: Array<dataScope>
+};
+
+const SelfEvaluation = (props: Props) => {
+
+	const { data } = props;
 
 	return (
-		<section className={'self-evaluation'}>
+		<Paper className='self-evaluation'>
 			<h2 className={'self-evaluation__heading'}>Self Evaluation</h2>
-			<h3 className={'self-evaluation__scope'}>Min scope 50</h3>
-			<ul className={'self-evaluation__list'}>
-				<SelfEvaluationListItem points={10} text={'Main page + page with a list of authors + author\'s page (only pages with content without widgets);'}/>
-				<SelfEvaluationListItem points={10} text={'Page with team members + page with worklog;'}/>
-				<SelfEvaluationListItem points={10} text={'Page with list of authors contains search widget;'}/>
-				<SelfEvaluationListItem points={20} text={'Portal has two languages;'}/>
-			</ul>
-			<h3 className={'self-evaluation__scope'}>Normal scope 140</h3>
-			<ul className={'self-evaluation__list'}>
-				<SelfEvaluationListItem points={20} text={'Portal has page with styleguide;'}/>
-				<SelfEvaluationListItem points={10} text={'Mobile version is okey;'}/>
-				<SelfEvaluationListItem points={10} text={'Ipad/tablet version is okey;'}/>
-				<SelfEvaluationListItem points={10} text={'Author\'s page contains timeline;'}/>
-				<SelfEvaluationListItem points={10} text={'Author\'s page contains video overlay;'}/>
-				<SelfEvaluationListItem points={20} text={'Author\'s page contains photo gallery;'}/>
-				<SelfEvaluationListItem points={10} text={'Author\'s page contains map (geowidget);'}/>
-				<SelfEvaluationListItem points={20} text={'Design (typography, icons, colors, links + buttons + input are styled);'}/>
-				<SelfEvaluationListItem points={20} text={'Material-ui / bootstrap is used;'}/>
-				<SelfEvaluationListItem points={10} text={'Portal has third language;'}/>
-			</ul>
-			<h3 className={'self-evaluation__scope'}>Extra scope 70</h3>
-			<ul className={'self-evaluation__list'}>
-				<SelfEvaluationListItem points={10} text={'Confidence of the project presentation;'}/>
-				<SelfEvaluationListItem points={10} text={'Project is made using gatsbyjs;'}/>
-				<SelfEvaluationListItem points={10} text={'Contentful / netlify cms is used for content management;'}/>
-				<SelfEvaluationListItem points={20} text={'Animations / special effects like paralax;'}/>
-				<SelfEvaluationListItem points={20} text={'Outstanding design;'}/>
-				<SelfEvaluationListItem points={20} text={'Storybook/styleguidist/other react styleguide tool usage for the page with styles.'}/>
-			</ul>
-		</section>
+			{
+				data.map((el) => {
+				const { items } = el;
+
+				return (
+					<div>
+						<h2 className='self-evaluation__scope'>{el.scope} - {el.points}</h2>
+						{items.map((item) => (
+							<SelfEvaluationListItem points={item.points} text={item.text} />
+						))}
+					</div>
+					);
+				})
+			}
+		</Paper>
 	);
-}
+};
 
 export default SelfEvaluation;
