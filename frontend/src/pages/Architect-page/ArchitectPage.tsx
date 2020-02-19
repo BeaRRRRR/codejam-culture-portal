463f43c59e1c79d17825e79756b7f361cb20fd33
madjack <kavanokami@gmail.com>
2020-02-19 19:09:43 +0400

    feat: add mapDispatch to ArchPage

--- frontend/src/pages/Architect-page/ArchitectPage.tsx
+++ frontend/src/pages/Architect-page/ArchitectPage.tsx
@@ -1,12 +1,16 @@
 import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { Dispatch } from "redux";
 
 import AuthorService from "../../services/authorService";
-
 import BasicInfo from "../../components/Basic-info";
 import Biography from "../../components/Biography";
 import WorksList from "../../components/Works-list";
 import Gallery from "../../components/Gallery";
 import Youtube from "../../components/Youtube";
+
+import { RootAction, AuthorModelExtended } from "../../store/types";
+import { actionTypes } from "../../actions";
 
 import "./ArchitectPage.scss";
 
@@ -30,4 +34,14 @@ function ArchitectPage() {
 	);
 }
 
-export default ArchitectPage;
+const mapDispatchToProps = (dispatch: Dispatch<RootAction>) => {
+	return {
+		fetchAuthor: (author: AuthorModelExtended) =>
+			dispatch({
+				type: actionTypes.FETCH_AUTHOR,
+				payload: author
+			})
+	};
+};
+
+export default connect(null, mapDispatchToProps)(ArchitectPage);
