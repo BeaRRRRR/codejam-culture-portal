5dc4f5de54dc818f40777ea48c1bfef7fa590edb
madjack <kavanokami@gmail.com>
2020-02-19 18:48:07 +0400

    feat: getAuthor method

--- frontend/src/services/authorService.ts
+++ frontend/src/services/authorService.ts
@@ -4,6 +4,12 @@ class AuthorService {
 			.then(res => res.json())
 			.catch(err => console.log(err));
 	}
+
+	getAuthor(id: string) {
+		return fetch(`/api/authors/:${id}`)
+			.then(res => res.json())
+			.catch(err => console.log(err));
+	}
 }
 
 export default AuthorService;
