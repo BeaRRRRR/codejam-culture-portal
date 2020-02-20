e20a5bff132f4d3518e366d6fa6451f6167932df
pburya <pburya@gmail.com>
2020-02-20 16:31:03 +0300

    feat: add 404 not found page

--- frontend/src/components/Description/Description.tsx
+++ frontend/src/components/Description/Description.tsx
@@ -3,19 +3,23 @@ import './Description.scss';
 
 function Description() {
 
-	return (
-		<section className={'description'}>
-			<h2 className={'description__heading'}>Description</h2>
-			<div className={'description__text'}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate deleniti dicta dolor, ea error harum molestias non provident sint, tempora tenetur velit voluptatem voluptatibus? Distinctio ducimus
-				officia quaerat repudiandae vero.
-			</div>
-			<div className={'description__text'}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est harum laboriosam obcaecati praesentium voluptatum. A consectetur dicta dignissimos dolor fuga illo natus nisi omnis quae repellendus? Iusto
-				laudantium
-				provident repudiandae. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus alias, architecto asperiores culpa cupiditate distinctio doloremque, error exercitationem in itaque laboriosam nostrum praesentium provident
-				quae quos suscipit tempora vitae voluptatum.
-			</div>
-		</section>
-	);
+		return (
+				<section className={'description'}>
+						<h2 className={'description__heading'}>Description</h2>
+						<div className={'description__text'}>Белорусскому союзу архитекторов уже исполнилось 70 лет. Его история — часть истории страны и народа, его созидательной деятельности. Пройден большой и сложный путь — путь поиска и воплощения, ошибок и
+								достижений. И сегодняшняя Беларусь являет миру свое прекрасное обновленное лицо благодаря таланту и умению, профессиональному мастерству Архитекторов, чьим неутомимым трудом на протяжении многих лет прирастала сокровищница
+								белорусского зодчества.
+						</div>
+						<div className={'description__text'}>Историческая хроника, запечатленная в документах, постановлениях, резолюциях, приоткрывает нам завесу времени, доносит стиль творчества и руководства, настроения и устремления нескольких поколений
+								зодчих. Все это прочитывается и между строк документов, и в авторских произведениях. В них — дух эпохи, сложной и неоднозначной, жизнь и творческие порывы людей, посвятивших себя Архитектуре. В них — история творческого содружества
+								людей. А над всем этим — дыхание Вечности.
+						</div>
+						<div className={'description__text'}>
+								Мы перевернули последнюю страницу хроники Белорусского Союза архитекторов. Но не последнюю в его деятельности. Время впишет новые имена, новые идеи, новые объекты. Но это — в будущем. А сегодня мы приносим искреннюю благодарность всем
+								архитекторам за созданное их трудом.
+						</div>
+				</section>
+		);
 }
 
 export default Description;
