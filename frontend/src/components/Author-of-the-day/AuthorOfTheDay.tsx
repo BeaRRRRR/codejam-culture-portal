e0af44b510bda4394d5f6a92f5ca5eaa00710004
pburya <pburya@gmail.com>
2020-02-21 00:11:43 +0300

    feat: make author of the day load data

--- frontend/src/components/Author-of-the-day/AuthorOfTheDay.tsx
+++ frontend/src/components/Author-of-the-day/AuthorOfTheDay.tsx
@@ -1,23 +1,55 @@
-import React from 'react';
-import './AuthorOfTheDay.scss';
+import React, {useEffect, useState} from 'react';
 import {Button, Card} from '@material-ui/core';
 import {Link} from 'react-router-dom';
+import AuthorService from '../../services/authorService';
+import Skeleton from '@material-ui/lab/Skeleton';
+
+import './AuthorOfTheDay.scss';
+
+function scrollTop() {
+		window.scrollTo(0, 0);
+}
 
 function AuthorOfTheDay() {
+		let [authorData, setAuthorData] = useState({
+				name: undefined,
+				birthdate: undefined,
+				deathDate: undefined,
+				summary: undefined,
+				pictureUrl: undefined
+		});
 
-		function scrollTop() {
-				window.scrollTo(0, 0);
-		}
+		const authorService = new AuthorService();
+
+		useEffect(() => {
+				authorService.getAuthor('5aXGlpoXkVSBdlBgNXDxwX')
+						.then((data) => {
+								setAuthorData(data);
+						});
+		}, []);
 
 		return (
 				<section className={'author-of-the-day'}>
 						<h2 className={'author-of-the-day__heading'}>Author of the day</h2>
 						<Card className={'author-of-the-day__content'}>
 								<div className={'author-of-the-day__column'}>
-										<h3>Михаил Павлович Парусников</h3>
-										<div>01.01.1970 — 01.01.2010</div>
-										<div className={'author-of-the-day__description'}>Cоветский архитектор, действительный член АА СССР, академик АН БССР. Педагог, профессор. Лауреат Государственной премии Белорусской ССР.</div>
-										<div>
+										<h3 className={'author-of-the-day__row'}>
+												{authorData.name ? authorData.name : <Skeleton variant='text' />}
+										</h3>
+										<div className={'author-of-the-day__row'}>
+												{authorData.name ? authorData.birthdate : <Skeleton variant='text' />}
+												— {authorData.name ? authorData.deathDate : <Skeleton variant='text' />}
+										</div>
+										<div className={'author-of-the-day__row author-of-the-day__description'}>
+												{authorData.name ? authorData.summary : <>
+														<Skeleton variant='text' />
+														<Skeleton variant='text' />
+														<Skeleton variant='text' />
+														<Skeleton variant='text' />
+												</>}
+										</div>
+										<div className={'author-of-the-day__row'}>
+												{/*TODO: change link path to accurate page*/}
 												<Link className={'author-of-the-day__link'} to='/architect/author1'>
 														<Button variant='contained' color='primary'>
 																<div onClick={scrollTop}>
@@ -28,7 +60,10 @@ function AuthorOfTheDay() {
 										</div>
 								</div>
 								<div className={'author-of-the-day__column'}>
-										<img src='https://grnkvch.github.io/CodeJam-Culture-Portal/static/baklanov-e8c0f280f4b86d29526072e70c9ee431.jpg' alt='architector of the day' />
+										{authorData.name ?
+												<img src={authorData.pictureUrl} alt='architector of the day' />
+												:
+												<Skeleton className={'author-of-the-day__image-skeleton'} variant='rect' width={200} height={250} />}
 								</div>
 						</Card>
 				</section>
