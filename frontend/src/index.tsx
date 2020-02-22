5688843f3b2d220642c1eb8c05662192a7758ced
madjack <kavanokami@gmail.com>
2020-02-22 16:20:19 +0400

    feat: add i18 to index

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,15 +1,23 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import { I18nextProvider } from "react-i18next";
+import i18next from "i18next";
 
 import store from "./store/store";
 import "./index.scss";
 
 import App from "./components/App";
 
+i18next.init({
+	interpolation: { escapeValue: false }
+});
+
 ReactDOM.render(
-	<Provider store={store}>
-		<App />
-	</Provider>,
+	<I18nextProvider i18n={i18next}>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</I18nextProvider>,
 	document.getElementById("example")
 );
