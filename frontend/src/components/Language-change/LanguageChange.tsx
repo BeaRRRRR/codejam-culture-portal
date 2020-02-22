9b5404bef9b89dc42c040b6cb181698e5d035e24
madjack <kavanokami@gmail.com>
2020-02-22 19:04:30 +0400

    feat: add interface to LanguageChange

--- frontend/src/components/Language-change/LanguageChange.tsx
+++ frontend/src/components/Language-change/LanguageChange.tsx
@@ -1,21 +1,43 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
 import ButtonGroup from "@material-ui/core/ButtonGroup";
+import { makeStyles, createStyles } from "@material-ui/core/styles";
+import { withTranslation } from "react-i18next";
 
 import "./LanguageChange.scss";
 
-function LanguageChange() {
+const useStyles = makeStyles(() =>
+	createStyles({
+		active: {
+			background: "red"
+		}
+	})
+);
+
+interface ILanguageChangeProps {
+	i18n: { changeLanguage: (lang: string) => object };
+}
+
+function LanguageChange(props: ILanguageChangeProps) {
+	const classes = useStyles();
+	const { i18n } = props;
+	console.log("buttons ", props);
 	return (
 		<ButtonGroup
 			className="button-group"
 			color="primary"
 			aria-label="outlined primary button group"
 		>
-			<Button>En</Button>
-			<Button>Ru</Button>
+			<Button
+				className={classes.active}
+				onClick={() => i18n.changeLanguage("en")}
+			>
+				En
+			</Button>
+			<Button onClick={() => i18n.changeLanguage("ru")}>Ru</Button>
 			<Button>Be</Button>
 		</ButtonGroup>
 	);
 }
 
-export default LanguageChange;
+export default withTranslation("common")(LanguageChange);
