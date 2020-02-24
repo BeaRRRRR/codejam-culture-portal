d34496fbaa18ef9bbeead7775fd9e649184e70a8
madjack <kavanokami@gmail.com>
2020-02-24 19:28:09 +0400

    feat: add themeChange to translation

--- frontend/src/components/Theme-change/ThemeChange.tsx
+++ frontend/src/components/Theme-change/ThemeChange.tsx
@@ -1,60 +1,67 @@
-import React, {MouseEvent} from 'react';
-import ButtonGroup from '@material-ui/core/ButtonGroup';
-import Button from '@material-ui/core/Button';
-import {createStyles, makeStyles} from '@material-ui/core/styles';
-import {switchTheme} from '../../actions';
-import {connect} from 'react-redux';
+import React, { MouseEvent } from "react";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+import Button from "@material-ui/core/Button";
+import { createStyles, makeStyles } from "@material-ui/core/styles";
+import { switchTheme } from "../../actions";
+import { connect } from "react-redux";
+import { compose } from "redux";
+import { withTranslation } from "react-i18next";
 
-import './ThemeChange.scss';
+import "./ThemeChange.scss";
 
 const useStyles = makeStyles(() =>
-		createStyles({
-				active: {
-						background: 'red'
-				}
-		})
+	createStyles({
+		active: {
+			background: "red"
+		}
+	})
 );
 
 function ThemeChange(props) {
-		const classes = useStyles();
-		let buttonNames = ['light', 'dark'];
-		let buttons: JSX.Element[] = [];
+	const { t } = props;
+	const classes = useStyles();
+	let buttonNames = ["light", "dark"];
+	let buttonText = [t("themeChange.light"), t("themeChange.dark")];
+	let buttons: JSX.Element[] = [];
 
-		function handleClick(e: MouseEvent<HTMLButtonElement>) {
-				const selectedTheme: string = e.currentTarget.value;
-				if (selectedTheme !== props.theme) {
-						switchTheme(selectedTheme);
-				}
+	function handleClick(e: MouseEvent<HTMLButtonElement>) {
+		const selectedTheme: string = e.currentTarget.value;
+		if (selectedTheme !== props.theme) {
+			switchTheme(selectedTheme);
 		}
+	}
 
-		buttonNames.map((buttonName) => {
-				buttons.push(
-						<Button
-								onClick={handleClick}
-								value={buttonName}
-								key={buttonName}
-								className={buttonName === props.theme ? classes.active : ''}
-						>
-								{buttonName}
-						</Button>
-				);
-		});
-
-		return (
-				<ButtonGroup
-						className='theme-change-button-group'
-						color='primary'
-						aria-label='outlined primary button group'
-				>
-						{buttons}
-				</ButtonGroup>
+	buttonNames.map((buttonName, i) => {
+		buttons.push(
+			<Button
+				onClick={handleClick}
+				value={buttonName}
+				key={buttonName}
+				className={buttonName === props.theme ? classes.active : ""}
+			>
+				{buttonText[i]}
+			</Button>
 		);
+	});
+
+	return (
+		<ButtonGroup
+			className="theme-change-button-group"
+			color="primary"
+			aria-label="outlined primary button group"
+		>
+			{buttons}
+		</ButtonGroup>
+	);
 }
 
 function mapStateToProps(oldState) {
-		return {
-				theme: oldState.theme
-		};
+	return {
+		theme: oldState.theme
+	};
 }
 
-export default connect(mapStateToProps)(ThemeChange);
+export default compose(
+	connect(mapStateToProps),
+	withTranslation("common")
+)(ThemeChange);
