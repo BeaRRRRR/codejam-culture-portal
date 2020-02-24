203be096ecf91e44dea57dc5b02f4bc9014f9afe
madjack <kavanokami@gmail.com>
2020-02-24 19:06:15 +0400

    feat: add selfEvaluation to translation

--- frontend/src/components/Self-evaluation/SelfEvaluation.tsx
+++ frontend/src/components/Self-evaluation/SelfEvaluation.tsx
@@ -1,43 +1,43 @@
-import React from 'react';
-import './SelfEvaluation.scss';
-import SelfEvaluationList from '../Self-evaluation-list';
-import { Paper } from '@material-ui/core';
+import React from "react";
+import "./SelfEvaluation.scss";
+import SelfEvaluationList from "../Self-evaluation-list";
+import { Paper } from "@material-ui/core";
 
 type dataScope = {
-	scope: string,
-	points: number,
-	items: Array<dataItem>
+	scope: string;
+	points: number;
+	items: Array<dataItem>;
 };
 
 type dataItem = {
-	points: number,
-	text: string
+	points: number;
+	text: string;
 };
 
 type Props = {
-	data: Array<dataScope>
+	data: Array<dataScope>;
+	t: (namespace: string) => string;
 };
 
 const SelfEvaluation = (props: Props) => {
-
-	const { data } = props;
+	const { data, t } = props;
 
 	return (
-		<section className='self-evaluation'>
-			<h2 className='self-evaluation__heading'>Self Evaluation</h2>
-			<Paper className='self-evaluation__paper'>
-				{
-					data.map((el, i) => {
-						const { items } = el;
+		<section className="self-evaluation">
+			<h2 className="self-evaluation__heading">{t("selfEvaluation")}</h2>
+			<Paper className="self-evaluation__paper">
+				{data.map((el, i) => {
+					const { items } = el;
 
-						return (
-							<div key={i}>
-								<h2 className='self-evaluation__scope'>{el.scope} - {el.points}</h2>
-								<SelfEvaluationList items={items} />
-							</div>
-						);
-					})
-				}
+					return (
+						<div key={i}>
+							<h2 className="self-evaluation__scope">
+								{el.scope} - {el.points}
+							</h2>
+							<SelfEvaluationList items={items} />
+						</div>
+					);
+				})}
 			</Paper>
 		</section>
 	);
