56f0cc11209be2b96e6bd2b69be08026bea82cf3
madjack <kavanokami@gmail.com>
2020-02-24 14:05:19 +0400

    feat: add translation to SearchBar

--- frontend/src/components/Search-bar/Search-bar.tsx
+++ frontend/src/components/Search-bar/Search-bar.tsx
@@ -1,99 +1,122 @@
-import React, { useEffect } from 'react';
-import { /*Link,*/ withRouter } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { Dispatch, compose } from 'redux';
-import { ReducerState, AuthorModel } from '../../store/types';
-import { fetchAuthorsList, RootAction } from '../../actions';
+import React, { useEffect } from "react";
+import { /*Link,*/ withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+import { Dispatch, compose } from "redux";
+import { withTranslation } from "react-i18next";
+import { ReducerState, AuthorModel } from "../../store/types";
+import { fetchAuthorsList, RootAction } from "../../actions";
 
-import Switch from '@material-ui/core/Switch';
-import {SearchField} from '../../styled-components';
-import {Typography, Paper, Grid, FormGroup, FormControlLabel, List, ListItem, ListItemText, ListItemProps } from '@material-ui/core';
-import Alert from '@material-ui/lab/Alert';
+import Switch from "@material-ui/core/Switch";
+import { SearchField } from "../../styled-components";
+import {
+	Typography,
+	Paper,
+	Grid,
+	FormGroup,
+	FormControlLabel,
+	List,
+	ListItem,
+	ListItemText,
+	ListItemProps
+} from "@material-ui/core";
+import Alert from "@material-ui/lab/Alert";
 
-import './Search-bar.scss';
-
-const SEARCH_PAGE_TITLE: string = 'Belarusian architects';
+import "./Search-bar.scss";
 
 interface SearchPanel {
 	authorsList: Array<AuthorModel>;
 	fetchAuthorsList: () => Array<AuthorModel>;
+	t: (namespace: string) => string;
 }
 
-const Search: React.FC<SearchPanel> = ({ authorsList, fetchAuthorsList }) => {
+const Search: React.FC<SearchPanel> = ({
+	authorsList,
+	fetchAuthorsList,
+	t
+}) => {
+	const SEARCH_PAGE_TITLE: string = t("search.title");
 	const [isByName, setIsByName] = React.useState(true);
-	const [term, setTerm] = React.useState('');
+	const [term, setTerm] = React.useState("");
 	useEffect(() => {
 		fetchAuthorsList();
 	}, [fetchAuthorsList]);
 
 	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-		setIsByName( e.target.checked );
+		setIsByName(e.target.checked);
 	};
 
 	const onLabelChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 		setTerm(e.target.value);
 	};
 
-	const ListItemLink = (props: ListItemProps<'a', { button?: true }>) => {
+	const ListItemLink = (props: ListItemProps<"a", { button?: true }>) => {
 		return <ListItem button component="a" {...props} />;
-	}
+	};
 
-	const visibleAuthors = authorsList.filter((author) => {
+	const visibleAuthors = authorsList.filter(author => {
 		if (term.length === 0) return true;
 		const { name, birthPlace } = author;
-		let variable = (isByName? name: birthPlace).toLowerCase();
-		return variable.includes(term.toLowerCase())
+		let variable = (isByName ? name : birthPlace).toLowerCase();
+		return variable.includes(term.toLowerCase());
 	});
 
 	const authors = visibleAuthors.map(author => {
 		const { id, name, birthPlace } = author;
 		return (
-			<ListItemLink key={id} className='author-list-item' href={`/#/architect/${id}`}>
-					<ListItemText color='primary' primary={`${name}, ${birthPlace}`}/>
+			<ListItemLink
+				key={id}
+				className="author-list-item"
+				href={`/#/architect/${id}`}
+			>
+				<ListItemText color="primary" primary={`${name}, ${birthPlace}`} />
 			</ListItemLink>
 		);
 	});
 
 	const noAuthorsMessage = (
-		<Alert severity="info">We have no authors for your request!</Alert>
-	)
+		<Alert severity="info">{t("search.notFound")}</Alert>
+	);
 
 	return (
-		<Grid className='search'
-				  container
-					direction="column"
-					justify="center"
-					alignItems="center"
-					spacing={10}>
+		<Grid
+			className="search"
+			container
+			direction="column"
+			justify="center"
+			alignItems="center"
+			spacing={10}
+		>
 			<Grid item xs={12}>
-				<Typography variant='h3' className='search__title'>{SEARCH_PAGE_TITLE}</Typography>
+				<Typography variant="h3" className="search__title">
+					{SEARCH_PAGE_TITLE}
+				</Typography>
 			</Grid>
 			<Grid item xs={12}>
-				<Paper className='search__form'>
+				<Paper className="search__form">
 					<FormGroup row>
 						<FormControlLabel
-							control={
-								<Switch
-									checked={isByName}
-									onChange={handleChange}
-								/>
-							}
-							label={'Search by'}
+							control={<Switch checked={isByName} onChange={handleChange} />}
+							label={t("search.searchBy")}
 						/>
 						<SearchField
-							label={`${isByName ? 'Name' : 'City'}`}
-							color='secondary'
-							variant='outlined'
-							onChange={onLabelChange}/>
+							label={`${
+								isByName ? t("search.searchByName") : t("search.searchByCity")
+							}`}
+							color="secondary"
+							variant="outlined"
+							onChange={onLabelChange}
+						/>
 					</FormGroup>
 				</Paper>
 			</Grid>
 			<Grid item xs={12}>
-				{ authors.length!==0 ? (
-					<Paper className='author-list'>
+				{authors.length !== 0 ? (
+					<Paper className="author-list">
 						<List>{authors}</List>
 					</Paper>
-				) : noAuthorsMessage }
+				) : (
+					noAuthorsMessage
+				)}
 			</Grid>
 		</Grid>
 	);
@@ -105,9 +128,8 @@ const mapStateToProps = (state: ReducerState): any => {
 	};
 };
 
-const mapDispatchToProps = (
-	dispatch: Dispatch<RootAction>
-): any => { // !!! fix type
+const mapDispatchToProps = (dispatch: Dispatch<RootAction>): any => {
+	// !!! fix type
 	return {
 		fetchAuthorsList: fetchAuthorsList(dispatch)
 	};
@@ -115,5 +137,6 @@ const mapDispatchToProps = (
 
 export default compose(
 	withRouter,
-	connect(mapStateToProps, mapDispatchToProps)
+	connect(mapStateToProps, mapDispatchToProps),
+	withTranslation("common")
 )(Search) as React.ComponentType;
