2ff5258e2d04ab7f1460109a985d5ab54843af16
pburya <pburya@gmail.com>
2020-02-26 13:49:10 +0300

    feat: update worklog

--- frontend/src/components/Time-spent/TimeSpent.tsx
+++ frontend/src/components/Time-spent/TimeSpent.tsx
@@ -5,46 +5,136 @@ import Box from '@material-ui/core/Box';
 import TimeSpentTable from '../Time-spent-table';
 
 function TimeSpent() {
-	return (
-		<Box component='section' className='time-spent'>
-			<Typography variant='h4' component='h2'>
-				Time spent
-			</Typography>
-			<TimeSpentTable developer={'Minitron'} features={
-				[
-					{
-						feature: 'create main page basic layout and file structure',
-						timeSpent: '3h',
-						startDate: '15.02',
-						endDate: '15.02'
-					},
-					{
-						feature: 'create worklog basic layout and component structure',
-						timeSpent: '3h',
-						startDate: '16.02',
-						endDate: '16.02'
-					}
-				]
-			} />
+		return (
+				<Box component='section' className='time-spent'>
+						<Typography variant='h4' component='h2'>
+								Time spent
+						</Typography>
+						<TimeSpentTable developer={'Alexander Fraltsov'} features={
+								[
+										{
+												feature: 'init search component',
+												timeSpent: '3h',
+												startDate: '15.02',
+												endDate: '15.02'
+										},
+										{
+												feature: 'added .editorconfig',
+												timeSpent: '0.25 h',
+												startDate: '15.02',
+												endDate: '15.02'
+										},
+										{
+												feature: 'studing and added material-ui in project',
+												timeSpent: '3h',
+												startDate: '16.02',
+												endDate: '16.02'
+										},
+										{
+												feature: 'studied styleguidist docs',
+												timeSpent: '5h',
+												startDate: '17.02',
+												endDate: '20.02'
+										},
+										{
+												feature: 'maked first styleguidist components',
+												timeSpent: '2h',
+												startDate: '21.02',
+												endDate: '21.02'
+										},
+										{
+												feature: 'added styleguidist to project',
+												timeSpent: '9h',
+												startDate: '22.02',
+												endDate: '22.02'
+										},
+										{
+												feature: 'added some styleguidist components',
+												timeSpent: '5h',
+												startDate: '23.02',
+												endDate: '23.02'
+										},
+										{
+												feature: 'implemented search component',
+												timeSpent: '2h',
+												startDate: '23.02',
+												endDate: '23.02'
+										},
+										{
+												feature: 'added some styleguidist components',
+												timeSpent: '3h',
+												startDate: '24.02',
+												endDate: '24.02'
+										},
+										{
+												feature: 'fixed search component styles',
+												timeSpent: '2h',
+												startDate: '24.02',
+												endDate: '24.02'
+										}
+								]
+						} />
 
-			<TimeSpentTable developer={'Minitron'} features={
-				[
-					{
-						feature: 'create main page basic layout and file structure',
-						timeSpent: '3h',
-						startDate: '15.02',
-						endDate: '15.02'
-					},
-					{
-						feature: 'create worklog basic layout and component structure',
-						timeSpent: '3h',
-						startDate: '16.02',
-						endDate: '16.02'
-					}
-				]
-			} />
-		</Box>
-	);
+						<TimeSpentTable developer={'Pavel Burya'} features={
+								[
+										{
+												feature: 'create main page basic layout and file structure',
+												timeSpent: '3h',
+												startDate: '15.02',
+												endDate: '15.02'
+										},
+										{
+												feature: 'create worklog basic layout and component structure',
+												timeSpent: '3h',
+												startDate: '16.02',
+												endDate: '16.02'
+										},
+										{
+												feature: 'main page material ui stylization',
+												timeSpent: '2h',
+												startDate: '17.02',
+												endDate: '17.02'
+										},
+										{
+												feature: 'main page data loading, create 404 page',
+												timeSpent: '3h',
+												startDate: '20.02',
+												endDate: '20.02'
+										},
+										{
+												feature: 'scroll top button',
+												timeSpent: '1h',
+												startDate: '23.02',
+												endDate: '23.02'
+										},
+										{
+												feature: 'theme changer',
+												timeSpent: '3h',
+												startDate: '24.02',
+												endDate: '24.02'
+										},
+										{
+												feature: 'resolve styleguide and redux conflict',
+												timeSpent: '2h',
+												startDate: '24.02',
+												endDate: '24.02'
+										},
+										{
+												feature: 'fix typescript errors, icons, markup, stylization',
+												timeSpent: '2,5h',
+												startDate: '25.02',
+												endDate: '26.02'
+										},
+										{
+												feature: 'worklog, presentation',
+												timeSpent: '0h',
+												startDate: '26.02',
+												endDate: '27.02'
+										}
+								]
+						} />
+				</Box>
+		);
 }
 
 export default TimeSpent;
